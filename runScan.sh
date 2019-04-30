6c5604c9c730e4d0828e909b214b444018f8f938
DyrellC <dyrell.chapman@iota.org>
2019-04-30 17:57:49 -0600

    runscan logs

--- runScan.sh
+++ runScan.sh
@@ -1,9 +1,17 @@
-echo "Downloading apt requirments "
+echo "Starting scans"
+
+echo ${pwd}
+echo ls
+
 source ./venv/bin/activate
 cd ixi/PerformanceScan
 
 echo "Startup Scan Started"
 echo $1
+
+echo "^^^^^^^^^^^^^"
+which pip
+which python
 
 nohup python3 .${CWD}/runScan.py -i 1 -n 180 -o ./Logs/ -c $1 &> Scan.log &
 
